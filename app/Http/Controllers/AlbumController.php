3237986f20485ebd1a6d0d60b3d8a645a3e98fde
Hai Nguyen <hainguyenhai0@gmail.com>
2023-03-01 22:43:29 +0700

    [Add] add somethings

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AlbumRequest;
+use App\Models\Album;
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AlbumRequest $request)
+    {
+        $album = Album::create($request->all());
+
+        return $album;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+        return $album;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(AlbumRequest $request, Album $album)
+    {
+        $album->update($request->all());
+
+        return $album;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        $album->delete();
+
+        return response('success', 204);
+    }
+}
